e351e6ca213c160aaa83b2d7e8fb847814fbb401
Mons Joseph <monsejoseph5@gmail.com>
2021-10-06 21:41:27 +0530

    Made changes with requests

--- ServerScript.js
+++ ServerScript.js
@@ -1,59 +1,35 @@
-const http = require("http");
-const fs = require("fs").promises;
+const express = require("express");
+const fs = require("fs");
 const statSync = require("fs").statSync;
 
-let myServer = http.createServer((req, res) => {
-    console.log("Connected");
-    res.writeHead(200, "successfully connected", { "Content-Type": "text/html" });
 
-    if (req.url == "/") {
-        fs.readFile("D:\\Mons\\Code Projects\\Node\\Task2\\index.html", "utf-8").then((data) => {
-            res.end(data);
-        }).catch((err) => {
-            if (err) {
-                res.end("error occured");
-                throw err;
-            }
-        });
-    } else if (req.url == "/files") {
-        console.log("requested files");
-        
-        //start
-        fs.readdir("D:\\Mons\\Code Projects\\Node\\Random Directory").then((files) => {
-            let data = "Name of files:- ";
+let myApp = express();
+
+myApp.get("/",(req,res)=>{
+  console.log("req url",req.url);
+  res.set('Content-Type', 'text/html')
+  fs.createReadStream("D:\\Mons\\Code Projects\\Local Repository\\Task2\\index.html").pipe(res);
+  //res.end();
+});
+
+myApp.get("/files",(req, res)=>{
+  console.log("req url",req.url);
+  fs.readdir("D:\\Mons\\Code Projects\\Node\\Random Directory",(err,files)=>{
+    if(err){
+      res.send("unknown error");
+      console.log("error reading directory");
+    }
+    else if(files){
+      let data = "Name of files:- ";
             for(file of files){
                 data += file +"("+ statSync(`D:\\Mons\\Code Projects\\Node\\Random Directory\\${file}`).size+" bytes"+")"+", ";
             }
             console.log(data);
-
-            let html = 
-            `<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <title>Document</title>
-                <style>
-                    .wrapper {
-                        position:absolute;
-                        left:35%;
-                        top:55%;
-                        margin-top:-150px;
-                        margin-left:-150px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="wrapper">
-                    <center>
-                        <p>${data}</p>
-                    </center>
-                </div>
-            </body>
-            </html>`;
-            res.end(html);
-        });
-        //end
-
+      res.json(data);
     }
+  });
 });
 
-myServer.listen(80);
+myApp.listen(80,()=>{
+  console.log("listening at port 80");
+});
