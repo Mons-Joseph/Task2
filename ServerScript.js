a69c15c188d33627ea1fff671554d3c250762f18
Mons Joseph <monsejoseph5@gmail.com>
2021-10-10 20:43:56 +0530

    Removed external package dependencies and added size convertion

--- ServerScript.js
+++ ServerScript.js
@@ -1,35 +1,46 @@
-const express = require("express");
+const http = require("http");
 const fs = require("fs");
 const statSync = require("fs").statSync;
 
+let myServer = http.createServer((req, res) => {
+  console.log("Connected");
+  res.writeHead(200, "successfully connected", { "Content-Type": "text/html" });
 
-let myApp = express();
+  if (req.url == "/" && req.method === "GET") {
+    fs.createReadStream("D:\\Mons\\Code Projects\\Local Repository\\Task2\\index.html").pipe(res);
+  }
 
-myApp.get("/",(req,res)=>{
-  console.log("req url",req.url);
-  res.set('Content-Type', 'text/html')
-  fs.createReadStream("D:\\Mons\\Code Projects\\Local Repository\\Task2\\index.html").pipe(res);
-  //res.end();
-});
+  if (req.url == "/files" && req.method === "GET") {
+    console.log("requested files");
 
-myApp.get("/files",(req, res)=>{
-  console.log("req url",req.url);
-  fs.readdir("D:\\Mons\\Code Projects\\Node\\Random Directory",(err,files)=>{
-    if(err){
-      res.send("unknown error");
-      console.log("error reading directory");
-    }
-    else if(files){
-      let data = "Name of files:- ";
-            for(file of files){
-                data += file +"("+ statSync(`D:\\Mons\\Code Projects\\Node\\Random Directory\\${file}`).size+" bytes"+")"+", ";
+    fs.readdir("D:\\Mons\\Code Projects\\Node\\Random Directory", (err, files) => {
+      if (err) {
+        res.send("unknown error");
+        console.log("error reading directory");
+      }
+      else if (files) {
+        let data = "Name of files:- ";
+        for (file of files) {
+
+          function sizeConverter(size){
+            if(size > 1024 && size < 1024 * 1024){
+              return size/1024 + "Kb";
+            }else if(size > 1024 * 1024){
+              return size/ (1024 * 1024) + "Mb"
+            }else{
+              return size + "bytes"
             }
-            console.log(data);
-      res.json(data);
-    }
-  });
+          }
+
+          let fileSize = statSync(`D:\\Mons\\Code Projects\\Node\\Random Directory\\${file}`).size
+          data += file + "(" + sizeConverter(fileSize) + ")" + ", ";
+        }
+        res.end(JSON.stringify(data));
+        console.log(data);
+      }
+    });
+
+  }
 });
 
-myApp.listen(80,()=>{
-  console.log("listening at port 80");
-});
+myServer.listen(80);
